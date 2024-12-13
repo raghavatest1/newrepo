3fdb0fecbc3b6c3fb864f7ea34c37c80dfbd85be
raghava <154610519+raghavatest1@users.noreply.github.com>
2024-12-13 17:39:29 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,5 +1,5 @@
 # Input two numbers from the user
-num1 = float(input("Enter the first number: "))
+num1 = float(input("Enter the first number: ")
 num2 = float(input("Enter the second number: "))
 
 # Add the numbers
